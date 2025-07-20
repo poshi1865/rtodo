5b9bc278bfec8365a67c2194d7bb450cb8c22da5
naachiket <naachiket.dev@gmail.com>
2025-07-20 15:17:49 +0530

    Complete argument parsing logic

--- src/main.rs
+++ src/main.rs
@@ -13,34 +13,29 @@ enum Operation {
     List,
     Add(String, Priority),
     Delete(u8),
-    Modify(u8)
+    Modify(u8, String),
+    Invalid
 }
 
-const USAGE_STRING: &'static str = "Usage string";
+const USAGE_STRING: &'static str = "<USAGE STRING>";
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let operation: Option<Operation> = parse_arguments(args);
-    match operation {
-        Some(_) => {},
-        None => {
-            println!("{}", USAGE_STRING);
-        }
-    }
+    let operation: Operation = parse_arguments(args).expect(USAGE_STRING);
 }
 
 fn parse_arguments(args: Vec<String>) -> Option<Operation> {
-    if args.len() < 3 {
-        //print usage
+    if args.len() == 1 {
+        return Some(Operation::List);
     }
-    match args.get(1)?.as_str() {
+    match args[1].as_str() {
         "a" => {
             let task_prompt: &str = "Task: ";
             let task: String = take_user_input_with_prompt(task_prompt);
             let mut priority_prompt: &str = "Assign Priority. 1. Critical, 2. High, 3. Medium, 4. Low: ";
 
             loop {
-                let priority: u8 = take_user_input_with_prompt(priority_prompt).parse().unwrap_or_else(|_| 5);
+                let priority: u8 = take_user_input_with_prompt(priority_prompt).parse().unwrap_or_else(|_| 255);
                 match priority {
                     1 => return Some(Operation::Add(task, Priority::Critical)),
                     2 => return Some(Operation::Add(task, Priority::High)),
@@ -50,17 +45,51 @@ fn parse_arguments(args: Vec<String>) -> Option<Operation> {
                 }
             }
         },
-        "-d" => {
+
+        "d" => {
+            let mut delete_prompt: &str = "Enter index to delete: ";
+            let mut correct_input: bool = true;
+
+            loop {
+                let index: u8 = take_user_input_with_prompt(delete_prompt)
+                    .parse()
+                    .unwrap_or_else(|_| {
+                        correct_input = false;
+                        255
+                    });
+                if correct_input {
+                    return Some(Operation::Delete(index));
+                }
+                delete_prompt = "Wrong input, try again: ";
+                correct_input = true;
+            }
         },
-        "-m" => {
+
+        "m" => {
+            let mut modify_prompt_index: &str = "Enter index to modify: ";
+            let mut correct_input: bool = true;
+            let modify_prompt_task: &str = "Enter modified task: ";
+
+            loop {
+                let index: u8 = take_user_input_with_prompt(modify_prompt_index)
+                    .parse()
+                    .unwrap_or_else(|_| {
+                        correct_input = false;
+                        255
+                    });
+                if correct_input {
+                    let task: String = take_user_input_with_prompt(modify_prompt_task);
+                    return Some(Operation::Modify(index, task));
+                }
+                modify_prompt_index = "Wrong input, try again: ";
+                correct_input = true;
+            }
         },
+
         _ => {
+            return Some(Operation::Invalid);
         }
     }
-
-    for arg in &args[1..] {
-    }
-    Some(Operation::List)
 }
 
 fn take_user_input_with_prompt(prompt: &str) -> String {
